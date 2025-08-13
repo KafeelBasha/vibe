90b76d9ca08a0ec7f0fd0c3d83205acb0ba35657
KafeelBasha <kafeel.basha15@gmail.com>
2025-08-13 18:30:45 +0530

    Update get_chunks.py

--- get_chunks.py
+++ get_chunks.py
@@ -3,18 +3,6 @@ from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core.utils import set_global_tokenizer
 from transformers import AutoTokenizer
 
-# def generate_chunks_context(text_content):
-#     # Initialize tokenizer and splitter
-#     set_global_tokenizer(AutoTokenizer.from_pretrained("hf-internal-testing/llama-tokenizer"))
-#     splitter = SentenceSplitter(chunk_size=1024, chunk_overlap=25)
-
-#     # Create Document objects and split into chunks
-#     documents = [Document(text=text) for text in text_content]
-#     nodes = splitter.get_nodes_from_documents(documents)
-#     chunks = [n.text for n in nodes]
-    
-#     return chunks[:30]
-
 # Initialize tokenizer and splitter once globally
 # or pass them as arguments to the function.
 try:
@@ -37,4 +25,5 @@ def generate_chunks_context(text_content):
     # Extract the text from the nodes
     chunks = [n.text for n in nodes]
     
+
     return chunks[:30]
